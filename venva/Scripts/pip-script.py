d853dc70e64aede5c843cdd718cd3feafdba8dcf
CWang24 <caowang34@gmail.com>
2015-09-12 22:49:39 -0700

    add venv

---
+++ venva/Scripts/pip-script.py
@@ -0,0 +1,9 @@
+#!C:\Languages\PythonScript\microblog\flask\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==7.1.2','console_scripts','pip'
+__requires__ = 'pip==7.1.2'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pip==7.1.2', 'console_scripts', 'pip')()
+)
